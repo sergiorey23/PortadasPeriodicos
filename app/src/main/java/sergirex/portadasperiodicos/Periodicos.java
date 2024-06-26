fe5f98f2366835d5f326e4594d272de8536f43ec
sergiorey23 <sergiorey@outlook.com>
2024-06-26 18:41:53 +0200

    New covers added

--- app/src/main/java/sergirex/portadasperiodicos/Periodicos.java
+++ app/src/main/java/sergirex/portadasperiodicos/Periodicos.java
@@ -9,14 +9,14 @@ class Periodicos {
     static final String [] deportes = {"marca.com", "as.com", "sport.es", "mundodeportivo.com", "superdeporte.es", "el9:lesportiudecatalunya.cat"};
     static final String [] economia = {"expansion.com", "eleconomista.es", "5dias:cincodias.elpais.com"};
     static final String [] locales = {"vozgalicia:lavozdegalicia.es", "diario_sevilla:diariodesevilla.es", "diario_informacion:informacion.es",
-            /*"correo_andalucia:elcorreoweb.es",*/ "20minutos_madrid:20minutos.es/madrid", "elperiodico_mediterraneo:elperiodicomediterraneo.com",
-            "levante:levante-emv.com", "diario_montanes:eldiariomontanes.es", "elcorreo:elcorreo.com", "diario_navarra:diariodenavarra.es",
-            "lanuevaespana:lne.es", "diario_cordoba:diariocordoba.es", "eldia_cordoba:eldiadecordoba.es", "granada_hoy:granadahoy.com", "ideal_jaen:ideal.es",
-            "nortecastilla_valladolid:elnortedecastilla.es","larioja:larioja.com", "diario_leon:diariodeleon.es", "diario_avila:diariodeavila.es",
-            "lasprovincias:lasprovincias.es", "farovigo:farodevigo.es", "laprovincia:laprovincia.es", "hoy_badajoz:hoy.es/extremadura",
-            "elperiodico_extremadura:elperiodicoextremadura.com", "latribuna_albacete:latribunadealbacete.es","latribuna_ciudadreal:latribunadeciudadreal.es",
-            "elfaro_ceuta:elfarodeceuta.es","elfaro_melilla:elfarodemelilla.es", "canarias7:canarias7.es", "diario_mallorca:diariodemallorca.es",
-            "diario_ibiza:diariodeibiza.es", "laverdad_murcia:laverdad.es" ,"heraldo_aragon:heraldo.es"};
+            /*"correo_andalucia:elcorreoweb.es",*/ "20minutos_madrid:20minutos.es/madrid", "20minutos_sevilla:20minutos.es/andalucia/sevilla", "viva_sevilla:vivasevilla.es",
+            "elperiodico_mediterraneo:elperiodicomediterraneo.com", "levante:levante-emv.com", "diario_montanes:eldiariomontanes.es", "elcorreo:elcorreo.com",
+            "diario_navarra:diariodenavarra.es", "lanuevaespana:lne.es", "diario_cordoba:diariocordoba.es", "eldia_cordoba:eldiadecordoba.es",
+            "granada_hoy:granadahoy.com", "ideal_jaen:ideal.es", "nortecastilla_valladolid:elnortedecastilla.es","larioja:larioja.com","diario_vasco:diariovasco.com",
+            "diario_leon:diariodeleon.es", "diario_avila:diariodeavila.es", "lasprovincias.es", "farodevigo.es", "diariosur.es", "malaga_hoy:malagahoy.es",
+            "laprovincia:laprovincia.es", "hoy_badajoz:hoy.es/extremadura", "elperiodico_extremadura:elperiodicoextremadura.com", "latribuna_albacete:latribunadealbacete.es",
+            "latribuna_ciudadreal:latribunadeciudadreal.es", "elfaro_ceuta:elfarodeceuta.es","elfaro_melilla:elfarodemelilla.es", "canarias7:canarias7.es",
+            "diario_mallorca:diariodemallorca.es", "diario_ibiza:diariodeibiza.es", "laverdad_murcia:laverdad.es" ,"heraldo_aragon:heraldo.es"};
     static final String [] internacional = {"newyork_times:nytimes.com/es:us", "wsj:wsj.com:us", "washington_post:washingtonpost.com:us","ft_uk:ft.com:uk",
             "sun:thesun.co.uk:uk", "lemonde:lemonde.fr:fr","spiegel:spiegel.de:de","sole24ore:ilsole24ore.com:it", "the_times:thetimes.co.uk:uk",
             "guardian:theguardian.com/uk:uk", "ar_clarin:clarin.com:ar", "ole:ole.com.ar:ar"};
